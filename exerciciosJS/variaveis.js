0977e5ffe76c0be7a96dc139e90b316a372d37ab
Adriano Dias <adrianodias7000@gmail.com>
2019-08-01 23:15:07 -0300

    Atualizando arquivo organizacao e add arq variaveis

---
+++ exerciciosJS/variaveis.js
@@ -0,0 +1,16 @@
+var a = 3
+var b = 10
+let d = 4
+
+
+c = a + b 
+console.log(c)
+console.log(a, b, c, d)
+
+
+a = 300
+b =400
+console.log(a, b, c, d)
+
+const e = 5
+console.log(e)
--- organizacao.js
+++ organizacao.js
@@ -1,3 +1,12 @@
 
 console.log("Hello, aprendendo JavaScript, Git e Angular.")
 console.log("Curos de JavaScript Cod3R.")
+
+//Cometario de uma linha
+console.log("Linha 2")
+
+/**
+ * Comentario de multiplas Linhas
+ * 
+ */
+console.log("Linha 3")
